8c468fbe68833e23d050d41446d182d1edf00abf
Shenghang Luo <shenghangluo@gmail.com>
2025-10-17 19:51:16 -0400

    insert sample person data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO persons_simple (name, birth_date, weight, height)
+VALUES
+    ('John Smith', '1990-05-15', 80.5, 180),
+    ('Sarah Johnson', '1995-08-22', 65.0, 165),
+    ('Mike Chen', '1988-03-10', 75.0, 175);
